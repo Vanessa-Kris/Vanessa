497501a0cf0ba7ef42dd33da9ae037e32dbe2346
Minion <nessakristo@gmail.com>
2022-11-11 15:25:33 +0100

    added useful links

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
 
 // Navbar 
 
